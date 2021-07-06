ddeffef01a09344b9457a055d00e2bd7402c303c
日瀬 健人 <kentnewbie@hotmail.com>
2021-07-06 18:27:14 +0900

    T

---
+++ SpareFiles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Aritiafel.Characters.Heroes;
+
+namespace SpareFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tina.SaveProject(ProjectChoice.AritiafelJS);            
+            Console.WriteLine("Project Files Spared.");
+            Console.ReadKey();
+        }
+    }
+}
