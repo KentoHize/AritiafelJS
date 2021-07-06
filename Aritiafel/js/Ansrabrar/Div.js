a544ea2edd3f6839e5193b73b7a438ec0fa25bda
日瀬 健人 <kentnewbie@hotmail.com>
2021-07-06 15:49:27 +0900

    U

--- Aritiafel/js/Ansrabrar/Div.js
+++ Aritiafel/js/Ansrabrar/Div.js
@@ -1,33 +1,9 @@
-﻿import { Style } from "./Style.js"
+﻿import { Element } from "./Element.js";
 
-class Div {
+class Div extends Element {
 
-    Width;
-    Height;
-    Styles;
-    Content;
-
-    constructor(content = null, styles = [], width = null, height = null) {
-        this.Styles = styles;
-        this.Content = content;
-        this.Width = width;
-        this.Height = height;
-    }
-
-    getObject(document) {
-        const div = document.createElement(`div`);
-        let styleStr = ``;
-        if (this.Width != null)
-            styleStr += ` width:${this.Width};`;
-        if (this.Height != null)
-            styleStr += ` height:${this.Height};`;
-        if (styleStr != ``)
-            div.style = styleStr;
-        if (this.Styles != null && this.Styles.length != 0)
-            div.className = Style.printClassName(this.Styles);
-        if (this.Content != null)
-            div.appendChild(this.Content.getObject(document, doc));
-        return div;
+    constructor(id = null, content = null, styles = [], width = null, height = null) {
+        super(`div`, id, content, styles, height);
     }
 }
 
---
+++ Aritiafel/js/Ansrabrar/Element.js
@@ -0,0 +1,41 @@
+﻿import { Style } from "./Style.js"
+
+class Element {
+
+    ID;
+    #Name;
+    Width;
+    Height;
+    Styles;
+    Content;
+
+    constructor(name, id = null, content = null, styles = [], width = null, height = null) {
+        this.#Name = name;
+        this.ID = id;
+        this.Content = content;
+        this.Styles = styles;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    getObject(document) {
+        const element = document.createElement(this.#Name);
+        let styleStr = ``;
+        if (this.ID != null && this.ID != ``)
+            element.id = this.ID;
+        if (this.Width != null)
+            styleStr += ` width:${this.Width};`;
+        if (this.Height != null)
+            styleStr += ` height:${this.Height};`;
+        if (styleStr != ``)
+            element.style = styleStr;
+        if (this.Styles != null && this.Styles.length != 0)
+            element.className = Style.printClassName(this.Styles);
+        if (this.Content != null)
+            element.appendChild(this.Content.getObject(document, doc));
+        return element;
+    }
+
+}
+
+export { Element };
